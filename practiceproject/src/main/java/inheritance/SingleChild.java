a599a3d74719b3a793eddca3a1a95b5f23e510ea
jinsh <jinsh@JINSHA>
2024-12-05 10:39:51 +0530

    My new commit

---
+++ practiceproject/src/main/java/inheritance/SingleChild.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class SingleChild extends SingleParent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingleChild obj=new SingleChild();
+		obj.add();
+
+	}
+
+}
---
+++ practiceproject/src/main/java/inheritance/SingleParent.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class SingleParent {
+	public void add()
+	{
+		int a=1;
+		int b=2;
+		int c=a+b;
+		System.out.println("Sum = "+c);
+	}
+
+}
